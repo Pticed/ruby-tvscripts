72f70b6cfaf737ee679ad079fb837dc198891d5b
Cedric Finance <pticedric@gmail.com>
2011-10-20 00:04:06 +0200

    Added a file scanner with options (ignored folders, accepted file extensions)

---
+++ lib/ruby-tvscripts/filescanner.rb
@@ -0,0 +1,42 @@
+module RubyTVScripts
+
+  class FileScanner
+
+    attr_accessor :ignored_folders, :accepted_extensions
+
+    def initialize options = {}
+      @ignored_folders = []
+      @accepted_extensions = []
+    end
+
+    def scan_files start_path
+      paths = [ start_path ]
+      
+      while path = paths.shift
+        if File.directory?(path)
+          next if folder_ignored?(path)
+          Dir.entries(path).reverse_each do |entry|
+            next if entry == "." or entry == ".."
+            paths.unshift File.join(path, entry)
+          end
+        else
+          next unless file_accepted?(path)
+          yield path
+        end
+      end
+    end
+
+    private
+
+    def folder_ignored?(path)
+      @ignored_folders.include? File.basename(path)
+    end
+
+    def file_accepted?(path)
+      @accepted_extensions.empty? or @accepted_extensions.any? { |extension| path.end_with?(extension) }
+    end
+
+
+  end
+
+end
---
+++ spec/filescanner_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+require 'ruby-tvscripts/filescanner'
+
+require 'fakefs/safe'
+require 'fakefs/file_system'
+
+include FakeFS
+
+describe RubyTVScripts::FileScanner do
+  
+  before(:all) do
+    @scanner = RubyTVScripts::FileScanner.new
+    FakeFS.activate!
+  end
+
+  before(:each) do
+    FileSystem.clear
+  end
+  
+  it 'should loop through the files in the given path' do
+    FileUtils.touch("/file1.txt")
+    FileUtils.touch("/file2.txt")
+    
+    files = []
+    @scanner.scan_files("/") do |file|
+      files << file
+    end
+
+    files.should == ["/file1.txt", "/file2.txt"]
+  end
+
+  it 'should not return directories' do
+    FileUtils.mkdir("/root")
+
+    files = []
+    @scanner.scan_files("/") do |file|
+      files << file
+    end
+
+    files.should be_empty
+  end
+
+  it 'should loop recursively through the files in the given path' do
+    FileUtils.touch("/file1.txt")
+    FileUtils.mkdir("/root")
+    FileUtils.touch("/root/file2.txt")
+    
+    files = []
+    @scanner.scan_files("/") do |file|
+      files << file
+    end
+
+    files.should == ["/file1.txt", "/root/file2.txt"]
+  end
+
+  it 'should ignore directories when asked' do
+    FileUtils.touch("/file1.txt")
+    FileUtils.mkdir("/root")
+    FileUtils.touch("/root/file2.txt")
+    FileUtils.mkdir("/root2")
+    FileUtils.touch("/root2/file3.txt")
+
+    @scanner.ignored_folders << "root"
+
+    files = []
+    @scanner.scan_files("/") do |file|
+      files << file
+    end
+
+    files.should == ["/file1.txt", "/root2/file3.txt"]
+  end
+
+  it 'should accept only the given file extensions when asked' do
+    FileUtils.touch("/file1.txt")
+    FileUtils.touch("/file2.xls")
+
+    @scanner.accepted_extensions << "txt"
+
+    files = []
+    @scanner.scan_files("/") do |file|
+      files << file
+    end
+
+    files.should == ["/file1.txt"]
+  end
+
+end
